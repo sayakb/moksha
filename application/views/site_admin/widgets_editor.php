b2cdd3dcbc5b52e84f15172ff0b0f4989b1b0c05
Sayak Banerjee <sayakb@kde.org>
2012-10-05 00:35:44 +0530

    Some fixes to the widget editor

--- application/views/site_admin/widgets_editor.php
+++ application/views/site_admin/widgets_editor.php
@@ -69,7 +69,7 @@
 	</div>
 <?= form_close() ?>
 
-<div id="modal-properties" class="modal fade hide" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true">
+<div id="modal-properties" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true">
 	<div class="modal-header">
 		<h3 id="modal-label"><?= $this->lang->line('control_properties') ?></h3>
 	</div>
